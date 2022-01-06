2871aca2d959ad545de2d2abc4f788d09de2ef8e
William James <wspr@gft.com>
2022-01-06 13:36:30 -0300

    FirstCommit

---
+++ william/william/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  william
+//
+//  Created by Pinto Junior, William James on 06/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
