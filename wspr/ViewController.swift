2dd226fe58f4e8ab737ec86b579fe5bb4627e42e
William James <wspr@gft.com>
2022-01-10 15:48:41 -0300

    change image by button

--- wspr/ViewController.swift
+++ wspr/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageViewChange: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    @IBAction func buttonChangerImage(_ sender: UIButton) {
+        let images = [ #imageLiteral(resourceName: "dexter"), #imageLiteral(resourceName: "Nick_Netflix_vertical"), #imageLiteral(resourceName: "mi"), #imageLiteral(resourceName: "panda"), #imageLiteral(resourceName: "macaco")]
+        imageViewChange.image = images.randomElement()
+    }
+    
 
 }
 
